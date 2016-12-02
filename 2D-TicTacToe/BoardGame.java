bc5264143f7c916c154966e90c724807140ac2c2
Michael F <michael.foe@live.com>
2016-12-01 19:14:10 -0500

    1.1

--- 2D-TicTacToe/BoardGame.java
+++ 2D-TicTacToe/BoardGame.java
@@ -3,13 +3,80 @@ package TicTacToe;
 public class BoardGame {
 	
 	private String[][] boardArray = new String[3][3];
-
-	public void setBoardArray(int position, String player){
-		//set position based on player
+	
+	public BoardGame(){
+		for(int x=0; x<3; x++){
+			for(int y=0; y<3; y++){
+				this.boardArray[x][y] = "-";
+			}
+		}
 	}
 	
-	public String[][] getBoardArray(){
-		return boardArray;
+	public int movesLeft(){
+		int countMoves = 0;
+		for(int i=0; i<3; i++){
+			for(int j=0; j<3; j++){
+				if(boardArray[i][j] == "-")
+					countMoves++;
+			}
+		}
+		return countMoves;
+	}
+	
+	public int winnerCheck(){
+		int winner = 3;
+		
+		//Check Rows
+		for(int i=0; i<3;i++){
+			if(boardArray[i][0] == "x" && boardArray[i][0] == boardArray[i][1] && boardArray[i][2] == boardArray[i][0]){
+				winner = 1;
+			}else if(boardArray[i][0] == "o" && boardArray[i][0] == boardArray[i][1] && boardArray[i][2] == boardArray[i][0]){
+				winner = 2;
+			}
+		}
+		
+		return winner;
+	}
+
+	public void setBoardArray(int position, int player){
+		//set position based on player
+		
+		String move;
+		
+		if(player == 1){
+			move = "x";
+		}else{
+			move = "o";
+		}
+		switch(position){
+			case 1:	boardArray[0][0] = move; 
+					break;
+			case 2: boardArray[0][1] = move;
+					break;
+			case 3: boardArray[0][2] = move;
+					break;
+			case 4: boardArray[1][0] = move;
+					break;
+			case 5: boardArray[1][1] = move;
+					break;
+			case 6: boardArray[1][2] = move;
+					break;
+			case 7: boardArray[2][0] = move;
+					break;
+			case 8: boardArray[2][1] = move;
+					break;
+			case 9: boardArray[2][2] = move;
+					break;
+			default: //do nothing
+				break;
+		}
+	}
+	
+	public String getBoardArray(){
+		String finalBoard = boardArray[0][0] + " | " + boardArray[0][1] + " | " + boardArray[0][2] +
+				"\n" + boardArray[1][0] + " | " + boardArray[1][1] + " | " + boardArray[1][2] +
+				"\n" + boardArray[2][0] + " | " + boardArray[2][1] + " | " + boardArray[2][2];
+		return finalBoard;
 	}
 	
 }
--- 2D-TicTacToe/mainGame.java
+++ 2D-TicTacToe/mainGame.java
@@ -3,29 +3,61 @@ import java.util.*;
 import java.util.Scanner;
 
 public class mainGame {
+	
+	
+	public static void nextMove(BoardGame x, int player){
+		int position=0;
+		Scanner user_input = new Scanner(System.in);
+		position = user_input.nextInt();
+		x.setBoardArray(position, player);
+		System.out.println(x.getBoardArray());
+	}
+	
+	public static void Start(BoardGame x){
+		System.out.println("Let's begin: Player1 (x) and Player2 (o)");
+		System.out.println("Please use number 1-9 to indicate your move.");
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//System.out.println("Hello World");
 
-		BoardGame start = new BoardGame();
+		BoardGame newGame = new BoardGame();
 		
-		Scanner user_input = new Scanner(System.in);;
-		int numOfPlayer = 0;
-		boolean validPlayer = true;
-		System.out.println("1 Player or 2 Player?");
-		while(validPlayer){
-			numOfPlayer = user_input.nextInt();
-			if(numOfPlayer != 2 || numOfPlayer !=1){
-				System.out.println("Invalid Entry");
-				numOfPlayer = user_input.nextInt();
+//		Scanner user_input = new Scanner(System.in);
+//		int numOfPlayer = 0;
+//		boolean validPlayer = true;
+//		System.out.println("1 Player or 2 Player?");
+//		do{
+//			numOfPlayer = user_input.nextInt();
+//			if(numOfPlayer != 2 || numOfPlayer !=1){
+//				System.out.println("Invalid Entry! Please enter either '1' or '2'.");
+//				numOfPlayer = user_input.nextInt();
+//			}else{
+//				validPlayer = false;
+//			}
+//		}while(validPlayer);
+//		
+//		user_input.close();
+		int movesRemaining=1, winner = 3;
+		int move = 1;
+		
+		Start(newGame);
+		while(movesRemaining != 0 && winner != 0){
+			nextMove(newGame, move);
+			if(move == 1){
+				move = 2;
 			}else{
-				validPlayer = false;
+				move = 1;
 			}
+			movesRemaining = newGame.movesLeft();
+			winner = newGame.winnerCheck();
+			System.out.println(winner);
 		}
 		
-		System.out.println(numOfPlayer);
+		
 	}
 
 }
